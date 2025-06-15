e5076e814db170bff9a2a440596d4e60e8bd52c7
eres2k <eeesener@amazon.com>
2025-06-15 18:35:26 +0200

    Update stackbit.config.js

--- stackbit.config.js
+++ stackbit.config.js
@@ -1,27 +1,14 @@
-import { defineStackbitConfig } from "@stackbit/types";
-import { GitContentSource } from "@stackbit/cms-git";
+// stackbit.config.js
+import { ContentfulContentSource } from "@stackbit/cms-contentful";
 
-export default defineStackbitConfig({
-  stackbitVersion: "~0.6.0",
+export default {
   contentSources: [
-    new GitContentSource({
-      rootPath: __dirname,
-      contentDirs: ["content"],
-      models: [
-        {
-          name: "Page",
-          type: "page",
-          urlPath: "/{slug}",
-          filePath: "content/pages/{slug}.json",
-          fields: [{ name: "title", type: "string", required: true }]
-        }
-      ],
-      assetsConfig: {
-        referenceType: "static",
-        staticDir: "public",
-        uploadDir: "images",
-        publicPath: "/"
-      }
+    new ContentfulContentSource({
+      spaceId: process.env.CONTENTFUL_SPACE_ID,
+      environment: process.env.CONTENTFUL_ENVIRONMENT,
+      previewToken: process.env.CONTENTFUL_PREVIEW_TOKEN,
+      accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN
     })
   ]
-});
+  // ...
+};
